280cf41f37426dcdd6a3ebaa9548187f35cf9eeb
Gabriele Cervelli <15055691+polyc@users.noreply.github.com>
2019-07-12 15:53:55 +0200

    refactors mqtt logic and changes baud rate to 9600

--- main.cpp
+++ main.cpp
@@ -13,12 +13,10 @@
  * Contributors:
  *    Ian Craggs - initial API and implementation and/or initial documentation
  *    Ian Craggs - make sure QoS2 processing works, and add device headers
- *    Gabriele Cervelli - Door's magnetic sensor 
- *    Giovanni De Luca - NFC handling
+ *    Gabriele Cervelli - Door's magnetic sensor
+ *    Giovanni De Luca - RFID handling
  *******************************************************************************/
 
-#define MQTTCLIENT_QOS1 0
-#define MQTTCLIENT_QOS2 0
 
 #include "mbed.h"
 #include "NTPClient.h"
@@ -26,43 +24,15 @@
 #include "MQTTmbed.h"
 #include "MQTTClient.h"
 #include "MQTT_server_setting.h"
-#include "mbed-trace/mbed_trace.h"
 #include "mbed_events.h"
 #include "mbedtls/error.h"
 
-#define LED_ON  MBED_CONF_APP_LED_ON
-#define LED_OFF MBED_CONF_APP_LED_OFF
-
 static volatile bool stopAlarm = false;
-
-/* Flag to be set when received a message from the server. */
-static volatile bool isMessageArrived = false;
-/* Buffer size for a receiving message. */
-const int MESSAGE_BUFFER_SIZE = 256;
-/* Buffer for a receiving message. */
-char messageBuffer[MESSAGE_BUFFER_SIZE];
 
 // An event queue is a very useful structure to debounce information between contexts (e.g. ISR and normal threads)
 // This is great because things such as network operations are illegal in ISR, so updating a resource in a button's fall() function is not allowed
 EventQueue eventQueue;
 Thread thread1;
-
-/*
- * Callback function called when a message arrived from server.
- */
-/*void messageArrived(MQTT::MessageData& md)
-{
-    // Copy payload to the buffer.
-    MQTT::Message &message = md.message;
-    if(message.payloadlen >= MESSAGE_BUFFER_SIZE) {
-        // TODO: handling error
-    } else {
-        memcpy(messageBuffer, message.payload, message.payloadlen);
-    }
-    messageBuffer[message.payloadlen] = '\0';
-
-    isMessageArrived = true;
-}*/
 
 /*
  * Callback function called when the button1 is clicked.
@@ -74,56 +44,61 @@ void btn1_rise_handler() {
 
 int main(int argc, char* argv[])
 {
-    mbed_trace_init();
-
     const float version = 0.9;
     bool isSubscribed = false;
 
-    //INIT PINs
-    DigitalIn  doorSensor(D1);
-    DigitalOut led(MBED_CONF_APP_LED_PIN, LED_ON);
 
+    //INIT PINs and LED
+    DigitalIn  doorSensor(D1);
+    DigitalOut led(LED2);
+    led = 0;
+
+    //Check on magnetic sensor
     if(doorSensor.is_connected()) {
-        printf("door sensor pin is connected and initialized! \n\r");
+        pc.printf("Door sensor pin is connected and initialized! \r\n");
     }
+    //Set magnetic sensor in PullUp mode
     doorSensor.mode(PullUp);
+    pc.printf("Pull up mode setted\n");
 
     NetworkInterface* network = NULL;
     MQTTNetwork* mqttNetwork = NULL;
     MQTT::Client<MQTTNetwork, Countdown>* mqttClient = NULL;
 
 
+    pc.printf("HelloMQTT: version is %.2f\r\n", version);
+    pc.printf("\r\n");
 
-
-    printf("HelloMQTT: version is %.2f\r\n", version);
-    printf("\r\n");
-
-    printf("Opening network interface...\r\n");
+    pc.printf("Opening network interface...\r\n");
     {
         network = NetworkInterface::get_default_instance();
         if (!network) {
-            printf("Error! No network inteface found.\n");
+            pc.printf("Error! No network inteface found.\n");
             return -1;
         }
 
-        printf("Connecting to network\n");
+        pc.printf("Connecting to network\n");
         nsapi_size_or_error_t ret = network->connect();
         if (ret) {
-            printf("Unable to connect! returned %d\n", ret);
+            pc.printf("Unable to connect! returned %d\n", ret);
             return -1;
         }
     }
-    printf("Network interface opened successfully.\r\n");
-    printf("\r\n");
+    pc.printf("Network interface opened successfully.\r\n");
+    pc.printf("\r\n");
+
+
 
     // sync the real time clock (RTC)
     NTPClient ntp(network);
     ntp.set_server("time.google.com", 123);
     time_t now = ntp.get_timestamp();
     set_time(now);
-    printf("Time is now %s", ctime(&now));
+    pc.printf("Time is now %s", ctime(&now));
 
-    printf("Connecting to host %s:%d ...\r\n", MQTT_SERVER_HOST_NAME, MQTT_SERVER_PORT);
+
+
+    pc.printf("Connecting to host %s:%d ...\r\n", MQTT_SERVER_HOST_NAME, MQTT_SERVER_PORT);
     {
         mqttNetwork = new MQTTNetwork(network);
         int rc = mqttNetwork->connect(MQTT_SERVER_HOST_NAME, MQTT_SERVER_PORT, SSL_CA_PEM,
@@ -132,22 +107,23 @@ int main(int argc, char* argv[])
             const int MAX_TLS_ERROR_CODE = -0x1000;
             // Network error
             if((MAX_TLS_ERROR_CODE < rc) && (rc < 0)) {
-                printf("ERROR from MQTTNetwork connect is %d.", rc);
+                pc.printf("ERROR from MQTTNetwork connect is %d.", rc);
             }
             // TLS error - mbedTLS error codes starts from -0x1000 to -0x8000.
             if(rc <= MAX_TLS_ERROR_CODE) {
                 const int buf_size = 256;
                 char *buf = new char[buf_size];
                 mbedtls_strerror(rc, buf, buf_size);
-                printf("TLS ERROR (%d) : %s\r\n", rc, buf);
+                pc.printf("TLS ERROR (%d) : %s\r\n", rc, buf);
             }
             return -1;
         }
     }
-    printf("Connection established.\r\n");
-    printf("\r\n");
+    pc.printf("Connection established.\r\n");
+    pc.printf("\r\n");
 
-    printf("MQTT client is trying to connect the server ...\r\n");
+
+    pc.printf("MQTT client is trying to connect the server ...\r\n");
     {
         MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
         data.MQTTVersion = 3;
@@ -158,32 +134,19 @@ int main(int argc, char* argv[])
         mqttClient = new MQTT::Client<MQTTNetwork, Countdown>(*mqttNetwork);
         int rc = mqttClient->connect(data);
         if (rc != MQTT::SUCCESS) {
-            printf("ERROR: rc from MQTT connect is %d\r\n", rc);
+            pc.printf("ERROR: rc from MQTT connect is %d\r\n", rc);
             return -1;
         }
     }
-    printf("Client connected.\r\n");
-    printf("\r\n");
+    pc.printf("Client connected.\r\n");
+    pc.printf("\r\n");
 
 
-    printf("Client is trying to subscribe a topic \"%s\".\r\n", MQTT_TOPIC_SUB);
-    {
-        int rc = mqttClient->subscribe(MQTT_TOPIC_SUB, MQTT::QOS0, messageArrived);
-        if (rc != MQTT::SUCCESS) {
-            printf("ERROR: rc from MQTT subscribe is %d\r\n", rc);
-            return -1;
-        }
-        isSubscribed = true;
-    }
-    printf("Client has subscribed a topic \"%s\".\r\n", MQTT_TOPIC_SUB);
-    printf("\r\n");
-
-    // Enable button 1
+    // Enable button 1 on the board as emergency alert shutdown
     InterruptIn btn1 = InterruptIn(MBED_CONF_APP_USER_BUTTON);
     btn1.rise(btn1_rise_handler);
 
-    // Turn off the LED to let users know connection process done.
-    led = LED_OFF;
+
 
     while(1) {
         /* Check connection */
@@ -194,12 +157,6 @@ int main(int argc, char* argv[])
         if(mqttClient->yield() != MQTT::SUCCESS) {
             break;
         }
-        /* Received a control message. */
-        /*if(isMessageArrived) {
-            isMessageArrived = false;
-            // Just print it out here.
-            printf("\r\nMessage arrived:\r\n%s\r\n\r\n", messageBuffer);
-        }*/
         /* Publish data */
 
         // IF DOOR IS OPEN
@@ -208,7 +165,7 @@ int main(int argc, char* argv[])
             char payload[] = "{ \"payload\": TWITTER-ID-GOES-HERE }";
 
             //START LED ALERT (A SOUND ALERT COULD BE IMPLEMENTED AS WELL)
-            led = LED_ON;
+            led = 1;
 
             MQTT::Message message;
             message.retained = false;
@@ -222,34 +179,42 @@ int main(int argc, char* argv[])
             message.id = id++;
             int ret = snprintf(buf, buf_size, "%s", payload);
             if(ret < 0) {
-                printf("ERROR: snprintf() returns %d.", ret);
+                pc.printf("ERROR: snprintf() returns %d.", ret);
                 continue;
             }
             message.payloadlen = ret;
             // Publish a message.
-            printf("Publishing message.\r\n");
+            pc.printf("Publishing message.\r\n");
             int rc = mqttClient->publish(MQTT_TOPIC_SUB, message);
             if(rc != MQTT::SUCCESS) {
-                printf("ERROR: rc from MQTT publish is %d\r\n", rc);
+                pc.printf("ERROR: rc from MQTT publish is %d\r\n", rc);
             }
-            printf("Message published.\r\n");
+            pc.printf("Message published.\r\n");
             delete[] buf;
 
+
             //check nfc for shutting off the alarm
-            while(!stopAlarm) {
+            pc.printf("Wait alert to stop\n");
+            while(! stopAlarm) {
                 wait(0.5);
             }
+            pc.printf("Alert stopped\n");
 
-            //reset led to off state
-            led = LED_OFF;
+            //reset led to off state and stopAlarm to false
+            led = 0;
+            stopAlarm = false;
 
+            //Wait until door is closed before restarting alarm, once it's shutted off
+            //the main cycle can restart checking door sensor again.
+            pc.printf("Wait door to be closed again\n");
             while(doorSensor.read() == 1) {
                 wait(0.5);
             }
         }
+        wait(0.5);
     }
 
-    printf("The client has disconnected.\r\n");
+    pc.printf("The client has disconnected.\r\n");
 
     if(mqttClient) {
         if(isSubscribed) {
